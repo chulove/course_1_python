911a83a2797eecbc30f71f5d9e4361702071689a
Tagir Bislimov <bislimovtagir@mail.ru>
2021-10-01 13:54:04 +0300

    Lesson 2

--- lesson_2/task_1.py
+++
@@ -1,5 +0,0 @@
-res_list = [None, True, 10, 10.0, 10 + 1j, 'abc', (1, 'a'), [[1, 2], 2, (1,)], {'a', 23.4, False},
-            {'abc': "ssss", 21: 7j, -1: ('re',)}]
-
-for i in res_list:
-    print(type(i))
--- lesson_2/task_4.py
+++
@@ -1,4 +0,0 @@
-text = input('Введите строку: ')
-words = text.split()
-for i, el in enumerate(words):
-    print(f'{i}: {el[:min(len(el), 10)]}')
